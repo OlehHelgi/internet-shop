c340e5f46d293ac1e81d89520f352d11d0a96135
Oleh Helgi <povic@ukr.net>
2025-07-01 17:10:36 +0300

    added reviews.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,30 @@
 const swiper = new Swiper('.accessories__slider', {
-    slidesPerView: 3,
-    spaceBetween: 40,
-    loop: true,
-    observer: true,
-    observeParents: true,
-    speed: 800,
-    navigation: {
-      nextEl: '.arrow-next',
-      prevEl: '.arrow-prev',
-    },
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+  observer: true,
+  observeParents: true,
+  speed: 800,
+  navigation: {
+    nextEl: '.arrow-next',
+    prevEl: '.arrow-prev',
+  },
 
-  });
+});
+
+const swiperReviews = new Swiper('.reviews-slider', {
+  slidesPerView: 9,
+  spaceBetween: 16,
+  loop: true,
+
+  navigation: {
+    nextEl: '.reviews-slider-next',
+    prevEl: '.reviews-slider-prev',
+  },
+
+  pagination: {
+    el: ".reviews-slider-pagination",
+    type: "fraction",
+  },
+
+});
